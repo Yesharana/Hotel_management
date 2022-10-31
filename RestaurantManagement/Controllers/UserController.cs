3657802bcf6ff16c74111fb51403fcc0fb5ae454
Manav-Mistry <msmistry07@gmail.com>
2022-10-31 16:35:20 +0530

    initial commit

---
+++ RestaurantManagement/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantManagement.Data;
+using RestaurantManagement.Models;
+
+namespace RestaurantManagement.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Create()
+        {
+            User user = new User();
+            return View(user);
+        }
+        
+
+        public IActionResult CreateUser(User usr)
+        {
+            _context.Users.Add(usr);
+            _context.SaveChanges();
+
+            HttpContext.Session.SetString("SUserName", usr.UserName);
+            HttpContext.Session.SetString("SUserId", usr.UserId.ToString());
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ViewUsers()
+        {
+            List<User> users = new List<User>();
+            users = _context.Users.ToList();
+            return View(users);
+        }
+
+        public IActionResult DeleteUser(int userId)
+        {
+            User usr = _context.Users.Find(userId);
+            if (usr != null)
+            {
+                _context.Users.Remove(usr);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(ViewUsers));
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult LoginUser(string email, string password)
+        {
+            //var user = _context.Users.;
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ RestaurantManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantManagement.Models;
+
+namespace RestaurantManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
